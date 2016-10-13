5a7f4c9c8dd328fb504cf29d5f40e396f7a343c1
Alex Chaphiv <achaphiv@gmail.com>
2016-10-13 05:53:47 -0400

    Extract out emails

--- src/main/java/businesscard/parser/impl/BusinessCardParserImpl.java
+++ src/main/java/businesscard/parser/impl/BusinessCardParserImpl.java
@@ -1,5 +1,9 @@
 package businesscard.parser.impl;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import businesscard.ContactInfo;
 import businesscard.EmailAddress;
 import businesscard.Name;
@@ -7,12 +11,25 @@ import businesscard.PhoneNumber;
 import businesscard.parser.BusinessCardParser;
 
 public final class BusinessCardParserImpl implements BusinessCardParser {
+  private final Pattern email = Pattern.compile("[.\\w]++@[.\\w]++");
+
   @Override
   public ContactInfo getContactInfo(String document) {
     return new ContactInfo(
         new Name(""),
         new PhoneNumber(""),
-        new EmailAddress("")
+        new EmailAddress(onlyMatch(email, document).orElse(""))
     );
+  }
+
+  private static Optional<String> onlyMatch(Pattern regex, String text) {
+    Matcher m = regex.matcher(text);
+    if (m.find()) {
+      String match = m.group();
+      if (!m.find()) {
+        return Optional.of(match);
+      }
+    }
+    return Optional.empty();
   }
 }
