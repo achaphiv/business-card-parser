b25de8d6e8803e90a642d24b6182cc3d5d7a8847
Alex Chaphiv <achaphiv@gmail.com>
2016-10-18 04:08:30 -0400

    Identify names via opennlp

--- src/main/java/businesscard/parser/impl/BusinessCardParserImpl.java
+++ src/main/java/businesscard/parser/impl/BusinessCardParserImpl.java
@@ -1,25 +1,85 @@
 package businesscard.parser.impl;
 
 import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 
 import businesscard.ContactInfo;
 import businesscard.EmailAddress;
 import businesscard.Name;
 import businesscard.PhoneNumber;
 import businesscard.parser.BusinessCardParser;
+import opennlp.tools.namefind.TokenNameFinder;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.util.Span;
 
 public final class BusinessCardParserImpl implements BusinessCardParser {
+  private final Tokenizer tokenizer;
+  private final TokenNameFinder nameFinder;
+  private final Pattern newLine = Pattern.compile("\r?\n");
   private final Pattern email = Pattern.compile("[.\\w]++@[.\\w]++");
+
+  public BusinessCardParserImpl(Tokenizer tokenizer, TokenNameFinder names) {
+    this.tokenizer = tokenizer;
+    this.nameFinder = names;
+  }
 
   @Override
   public ContactInfo getContactInfo(String document) {
     return new ContactInfo(
-        new Name(""),
+        new Name(mostLikelyName(document).orElse("")),
         new PhoneNumber(""),
         new EmailAddress(onlyMatch(email, document).orElse(""))
     );
+  }
+
+  private Optional<String> mostLikelyName(String document) {
+    Queue<PossibleName> all = possibleNames(document);
+    return !all.isEmpty() ? Optional.of(all.peek().value) : Optional.empty();
+  }
+
+  /**
+   * Ordered by most likely to least likely.
+   */
+  private Queue<PossibleName> possibleNames(String document) {
+    Queue<PossibleName> found = new PriorityQueue<>();
+    for (String sentence : newLine.split(document)) {
+      String[] foundTokens = tokenizer.tokenize(sentence);
+      Span[] spans = nameFinder.find(foundTokens);
+      for (Span span : spans) {
+        PossibleName maybe = new PossibleName(
+            span.getProb(),
+            Joiner.on(" ").join(Span.spansToStrings(spans, foundTokens))
+        );
+        found.add(maybe);
+      }
+    }
+    return found;
+  }
+
+  private static final class PossibleName implements Comparable<PossibleName> {
+    private final double probability;
+    final String value;
+
+    PossibleName(double probability, String value) {
+      this.probability = probability;
+      this.value = value;
+    }
+
+    @Override
+    public int compareTo(PossibleName o) {
+      return ComparisonChain.start()
+          // Larger probability first
+          .compare(probability, o.probability, Ordering.natural().reverse())
+          .compare(value, o.value)
+          .result();
+    }
   }
 
   private static Optional<String> onlyMatch(Pattern regex, String text) {
--- src/test/java/businesscard/parser/impl/BusinessCardParserImplIT.java
+++ src/test/java/businesscard/parser/impl/BusinessCardParserImplIT.java
@@ -2,18 +2,32 @@ package businesscard.parser.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
 
 import businesscard.ContactInfo;
 import businesscard.EmailAddress;
 import businesscard.Name;
 import businesscard.PhoneNumber;
+import businesscard.parser.BusinessCardParser;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 @RunWith(Parameterized.class)
 public final class BusinessCardParserImplIT {
@@ -78,6 +92,26 @@ public final class BusinessCardParserImplIT {
     return Arrays.asList(all);
   }
 
+  @ClassRule
+  public static final TemporaryFolder tmpDir = new TemporaryFolder();
+
+  private static File tokensModel;
+  private static File personModel;
+
+  @BeforeClass
+  public static void retrieveModels() throws Exception {
+    tokensModel = tmpDir.newFile();
+    personModel = tmpDir.newFile();
+    model("en-token.bin").copyTo(Files.asByteSink(tokensModel));
+    model("en-ner-person.bin").copyTo(Files.asByteSink(personModel));
+  }
+
+  private static ByteSource model(String name) throws Exception {
+    return Resources.asByteSource(
+        new URL("http://opennlp.sourceforge.net/models-1.5/" + name)
+    );
+  }
+
   private final String input;
   private final ContactInfo expected;
 
@@ -102,6 +136,10 @@ public final class BusinessCardParserImplIT {
   }
 
   private ContactInfo actual() throws Exception {
-    return new BusinessCardParserImpl().getContactInfo(input);
+    BusinessCardParser toTest = new BusinessCardParserImpl(
+        new TokenizerME(new TokenizerModel(tokensModel)),
+        new NameFinderME(new TokenNameFinderModel(personModel))
+    );
+    return toTest.getContactInfo(input);
   }
 }
